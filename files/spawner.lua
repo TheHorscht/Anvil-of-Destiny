c58c467185bf7bdfd93e5dfd28a8ea860fc422d6
The Horscht <realhorscht+github@gmail.com>
2019-10-27 19:36:46 +0100

    Organize files into folders and update references

--- files/spawner.lua
+++ files/spawner.lua
@@ -4,11 +4,11 @@ RegisterSpawnFunction( 0xffff0047, "spawn_anvil")
 function spawn_anvil_altar( x, y )
 	local w = 184
 	local h = 96
-	LoadPixelScene( "mods/anvil_of_destiny/files/altar.png", "mods/anvil_of_destiny/files/altar_visual.png", x - (w / 2), y - h + 1, "mods/anvil_of_destiny/files/altar_background.png", true )
+	LoadPixelScene("mods/anvil_of_destiny/files/entities/altar/altar.png", "mods/anvil_of_destiny/files/entities/altar/altar_visual.png", x - (w / 2), y - h + 1, "mods/anvil_of_destiny/files/entities/altar/altar_background.png", true )
 end
 
 function spawn_anvil( x, y )
 	local w = 78
 	local h = 37
-	EntityLoad("mods/anvil_of_destiny/files/entities/anvil.xml", x, y - (h / 2))
+	EntityLoad("mods/anvil_of_destiny/files/entities/anvil/anvil.xml", x, y - (h / 2))
 end
