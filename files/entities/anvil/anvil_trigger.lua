10a71294c1f3a6c8e4a8d7185da941f86d320aaf
The Horscht <realhorscht+github@gmail.com>
2019-10-30 00:09:30 +0100

    Make anvil accept tablets to add perma spell

--- files/entities/anvil/anvil_trigger.lua
+++ files/entities/anvil/anvil_trigger.lua
@@ -1,4 +1,7 @@
-dofile( "data/scripts/lib/utilities.lua" )
+dofile("data/scripts/lib/utilities.lua")
+dofile("mods/anvil_of_destiny/files/wand_utils.lua")
+dofile("data/scripts/gun/gun_enums.lua")
+
 
 if g_mymod_anvil_state == nil then
   g_mymod_anvil_state = {}
@@ -58,7 +61,6 @@ function collision_trigger(colliding_entity_id)
         GamePlaySound("mods/anvil_of_destiny/fmod/Build/Desktop/my_mod_audio.snd", "snd_mod/jingle", x, y)
       elseif get_state().wands_sacrificed == 2 then
         EntitySetComponentsWithTagEnabled(entity_id, "emitter2", true)
-        -- TODO: Localize this        
         GamePrintImportant("A gift from the gods", "")
         GamePlaySound("mods/anvil_of_destiny/fmod/Build/Desktop/my_mod_audio.snd", "snd_mod/fanfare", x, y)
         edit_component(entity_id, "AudioLoopComponent", function(comp, vars)
@@ -69,12 +71,43 @@ function collision_trigger(colliding_entity_id)
         if wand_level_to_spawn > 6 then
           wand_level_to_spawn = 6
         end
-        EntityLoad("data/entities/items/wand_level_0" .. wand_level_to_spawn .. ".xml", x + 4, y - 25)
-        edit_component(entity_id, "CollisionTriggerComponent", function(comp, vars)
+        local wand_type = "level"
+        if Random() < 0.5 then
+          wand_type = "unshuffle"
+        end
+        local generated_wand = EntityLoad("data/entities/items/wand_"..wand_type.."_0"..wand_level_to_spawn..".xml", x + 4, y - 25)
+        if get_state().powered_up then
+          local rand_spell_roll = Random()
+          local always_cast_spell = nil
+          if rand_spell_roll < 0.50 then
+            always_cast_spell = GetRandomActionWithType(x, y, wand_level, ACTION_TYPE_PROJECTILE)
+          elseif rand_spell_roll < 0.80 then
+            always_cast_spell = GetRandomActionWithType(x, y, wand_level, ACTION_TYPE_MODIFIER)
+          else
+            always_cast_spell = GetRandomActionWithType(x, y, wand_level, ACTION_TYPE_DRAW_MANY)
+          end
+          if always_cast_spell == nil then
+            GamePrint("Anvil of Destiny could not find a spell for your wand!")
+          else
+            -- If lowest sacrificed wand level is below 6, do not add another always cast spell if the generated wand already has one
+            -- otherwise, get a chance of a second permanent spell!
+            if get_state().level_low >= 6 or wand_get_attached_spells_count(generated_wand) == 0 then
+              GamePrint("Adding more power!")
+              --debug_print_table(always_cast_spell)
+              AddGunActionPermanentSafely(generated_wand, always_cast_spell)
+            end
+          end
+        end
+        edit_all_components(entity_id, "CollisionTriggerComponent", function(comp, vars)
           EntityRemoveComponent(entity_id, comp)
         end)
       end
+    elseif EntityHasTag(colliding_entity_id, "tablet") then
+      if not get_state().powered_up then
+        EntitySetComponentsWithTagEnabled(entity_id, "emitter_powered_up", true)
+        get_state().powered_up = true
+        EntityKill(colliding_entity_id)
+      end
     end
-    
   end
 end
---
+++ files/wand_utils.lua
@@ -0,0 +1,54 @@
+dofile("data/scripts/gun/procedural/gun_action_utils.lua")
+-- dofile("data/scripts/lib/utilities.lua")
+
+function AddGunActionPermanentSafely(wand_id, spell)
+	-- Extend slots
+	local ability_comp = wand_get_ability_component(wand_id)
+	if ability_comp then
+		local capacity = ComponentObjectGetValue(ability_comp, "gun_config", "deck_capacity")
+		local capacity = ComponentObjectSetValue(ability_comp, "gun_config", "deck_capacity", capacity + 1)
+		AddGunActionPermanent(wand_id, spell)
+	else
+		local error_msg = "Error: AddGunActionPermanentSafely() - AbilityComponent not found!"
+		GamePrint(error_msg)
+		print(error_msg)
+	end
+end
+
+function wand_get_attached_spells_count(wand_id)
+	local children = EntityGetAllChildren(wand_id)
+	local count = 0
+	for i,v in ipairs(children) do
+		local all_comps = EntityGetAllComponents(v)
+		for i, c in ipairs(all_comps) do
+			if ComponentGetValue(c, "permanently_attached") == "1" then
+				count = count + 1
+			end
+		end
+	end
+	return count
+end
+
+function wand_get_ability_component(wand_id)
+	local components = EntityGetAllComponents(wand_id)
+	for i, component_id in ipairs(components) do
+		for k, v2 in pairs(ComponentGetMembers(component_id)) do
+			if(k == "mItemRecoil") then
+				return component_id
+			end
+		end
+	end
+end
+
+function wand_get_spells_count(wand_id)
+	local num_children = #EntityGetAllChildren(wand_id)
+	return num_children - wand_get_attached_spells_count(wand_id)
+end
+
+function wand_get_free_slots(wand_id)
+	local ability_comp = wand_get_ability_component(wand_id)
+	if ability_comp then
+		local capacity = ComponentObjectGetValue(ability_comp, "gun_config", "deck_capacity")
+		return (capacity - (wand_get_spells_count(wand_id) + wand_get_attached_spells_count(wand_id)))
+	end
+end
