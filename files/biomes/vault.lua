d5ae37a54e6ec2b91c5f8eeea7d626afddf7eafa
The Horscht <realhorscht+github@gmail.com>
2019-11-01 12:05:15 +0100

    Add vertical altar room for vault

--- files/biomes/vault.lua
+++ files/biomes/vault.lua
@@ -2,10 +2,17 @@ dofile("mods/anvil_of_destiny/files/spawner.lua")
 dofile("mods/anvil_of_destiny/files/probability_util.lua")
 dofile("mods/anvil_of_destiny/anvil_of_destiny_config.lua")
 --[[ This is a scene which loads the altar pixel scene which loads the anvil ]]
+table.insert(g_pixel_scene_01, {
+  prob   		    	= get_probability_value_for_inserting(g_pixel_scene_01, config_altar_room_spawn_probability_vault),
+  material_file 	= "mods/anvil_of_destiny/files/altar_loader_vault_v.png",
+  visual_file	  	= "",
+  background_file	= "",
+  is_unique		    = 0
+})
 table.insert(g_pixel_scene_02, {
   prob   		    	= get_probability_value_for_inserting(g_pixel_scene_02, config_altar_room_spawn_probability_vault),
-  material_file 	= "mods/anvil_of_destiny/files/altar_loader_vault.png",
-  visual_file	  	= "mods/anvil_of_destiny/files/altar_loader_vault_visual.png",
+  material_file 	= "mods/anvil_of_destiny/files/altar_loader_vault_h.png",
+  visual_file	  	= "mods/anvil_of_destiny/files/altar_loader_vault_h_visual.png",
   background_file	= "",
   is_unique		    = 0
 })
--- files/debug/debug_room_vault_h.lua
+++ files/debug/debug_room_vault_h.lua
@@ -7,5 +7,5 @@ function init( x, y, w, h )
   local biome_chunk_size = 512
   local wang_w = 400
   local wang_h = wang_w / 2
-  LoadPixelScene("mods/anvil_of_destiny/files/altar_loader_vault.png", "mods/anvil_of_destiny/files/altar_loader_vault_visual.png", x + (biome_chunk_size / 2) - (wang_w / 2), y + (biome_chunk_size / 2) - (wang_h / 2), "", true)
+  LoadPixelScene("mods/anvil_of_destiny/files/altar_loader_vault_h.png", "mods/anvil_of_destiny/files/altar_loader_vault_h_visual.png", x + (biome_chunk_size / 2) - (wang_w / 2), y + (biome_chunk_size / 2) - (wang_h / 2), "", true)
 end
