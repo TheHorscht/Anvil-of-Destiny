afc32f40910c41970eed5eb3e3c336e045fdbfa0
The Horscht <realhorscht+github@gmail.com>
2019-11-03 19:32:33 +0100

    Add some ideas

--- TODO.md
+++ TODO.md
@@ -1,6 +1,19 @@
 - Indicate what level wands have?
 - Change the wand level logic?
 - Make anvil indestructible by black holes etc
+- Change how occurence rates work AGAIN, instead of "1.5 anvils per coalmine" use "0.2 anvils per chunk"
+- IDEA: 2 tablets + 1 wand = improve that wands stats
+
+
+try:
+-- LifetimeComponent
+-- PixelSceneComponent
+-- RotateTowardsComponent
+-- TeleportComponent
+-- VariableStorageComponent
+-- VelocityComponent
+-- magic number DEBUG_NO_PAUSE_ON_WINDOW_FOCUS_LOST
+
 
 - Once it exists in the game, maybe use ParticleEmitterComponent::use_rotation_from_entity in anvil.xml
   (On hold since it doesn't rotate now anyway)
--- files/wand_utils.lua
+++ files/wand_utils.lua
@@ -52,3 +52,22 @@ function wand_get_free_slots(wand_id)
 		return (capacity - (wand_get_spells_count(wand_id) + wand_get_attached_spells_count(wand_id)))
 	end
 end
+
+-- by kermit tears
+function GetWandActions( wand )
+	local actions = {};
+	local children = EntityGetAllChildren( wand );
+	for i,v in ipairs( children ) do
+			local all_comps = EntityGetAllComponents( v );
+			local action_id = nil;
+			local permanent = false;
+			for i, c in ipairs( all_comps ) do
+					action_id = ComponentGetValueDefault( c, "action_id", action_id );
+					permanent = ComponentGetValueDefault( c, "permanently_attached", permanent );
+			end
+			if action_id ~= nil then
+					table.insert( actions, {action_id=action_id, permanent=permanent} );
+			end
+	end
+	return actions;
+end
