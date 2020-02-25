059b71958b33c5c8d9692bb3093937b10a214499
The Horscht <realhorscht+github@gmail.com>
2020-02-25 22:40:15 +0100

    Make bomb go 1,2,5,3 and refactor it

--- files/entities/countdown/tick.lua
+++ files/entities/countdown/tick.lua
@@ -1,42 +1,37 @@
-dofile("mods/anvil_of_destiny/lib/StringStore/stringstore.lua")
-dofile("mods/anvil_of_destiny/lib/StringStore/noitavariablestore.lua")
-
+dofile_once("data/scripts/lib/coroutines.lua")
+-- dofile_once("data/scripts/lib/utilities.lua")
 
 local entity_id = GetUpdatedEntityID()
 local x, y = EntityGetTransform(entity_id)
-local STATE = stringstore.open_store(stringstore.noita.variable_storage_components(entity_id))
+local sprite_component = EntityGetFirstComponent(entity_id, "SpriteComponent")
 
-STATE.tick = (STATE.tick or 0) + 1
-
-if STATE.tick <= 3 then
-  local sprite_component = EntityGetComponent(entity_id, "SpriteComponent")
-  if sprite_component ~= nil and sprite_component[1] ~= nil then
-    local number_to_show = tostring(STATE.tick)
-    if STATE.tick == 3 then
-      number_to_show = "5"
+async(function()
+  local sequence = {
+    { number=1, delay=0 },
+    { number=2, delay=0 },
+    { number=5, delay=0 },
+    { number=3, delay=45 },
+  }
+  for i, v in ipairs(sequence) do
+    if v.delay > 0 then
+      wait(v.delay)
     end
-    ComponentSetValue(sprite_component[1], "rect_animation", number_to_show)
-    ComponentSetValue(sprite_component[1], "alpha", "1")
-    if STATE.tick < 3 then
-      GamePlaySound("mods/anvil_of_destiny/audio/anvil_of_destiny.snd", "tick_low", x, y)
-    else
+    ComponentSetValue(sprite_component, "rect_animation", v.number)
+    ComponentSetValue(sprite_component, "alpha", 1)
+
+    if v.number == 5 then
       GamePlaySound("mods/anvil_of_destiny/audio/anvil_of_destiny.snd", "tick_high", x, y)
+    else
+      GamePlaySound("mods/anvil_of_destiny/audio/anvil_of_destiny.snd", "tick_low", x, y)
     end
-    local shrink_script_file = "mods/anvil_of_destiny/files/entities/countdown/shrink.lua"
-    local lua_components = EntityGetComponent(entity_id, "LuaComponent")
-    if lua_components ~= nil then
-      for i, lua_component in ipairs(lua_components) do
-        local script_source_file = ComponentGetValue(lua_component, "script_source_file")
-        if script_source_file == shrink_script_file then
-          EntityRemoveComponent(entity_id, lua_component)
-        end
-      end
+
+    local alpha = ComponentGetValue(sprite_component, "alpha")
+    while tonumber(alpha) > 0 do
+      wait(1)
+      alpha = ComponentGetValue(sprite_component, "alpha")
+      alpha = tonumber(alpha)
+      ComponentSetValue(sprite_component, "alpha", alpha - 0.035)
     end
-    EntityAddComponent(entity_id, "LuaComponent", {
-      script_source_file=shrink_script_file,
-      execute_on_added="1",
-      execute_every_n_frame="1",
-      execute_times="60"
-    })
+    ComponentSetValue(sprite_component, "alpha", 0)
   end
-end
+end)
