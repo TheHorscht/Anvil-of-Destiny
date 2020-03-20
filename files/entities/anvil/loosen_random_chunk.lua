96e2ff73de73ab64087a13007627b7abd5dbbf25
The Horscht <realhorscht+github@gmail.com>
2020-03-20 19:16:21 +0100

    Fix collapse masks path

--- files/entities/anvil/loosen_random_chunk.lua
+++ files/entities/anvil/loosen_random_chunk.lua
@@ -3,5 +3,5 @@ local x, y = EntityGetTransform(entity_id)
 
 SetRandomSeed(GameGetFrameNum(), GameGetFrameNum())
 for i=0, 3 do
-  LooseChunk(x + Random(-40, 40), y + Random(-20, 25), "data/collapse_masks/small/"..Random(0, 14)..".png")
+  LooseChunk(x + Random(-40, 40), y + Random(-20, 25), "data/procedural_gfx/collapse_small/"..Random(0, 14)..".png")
 end
