c2efdd3ae13f0166fb60e7443232fea39120234c
The Horscht <realhorscht+github@gmail.com>
2024-01-23 00:56:51 +0100

    Fix portable anvil activating in inventory when kicking

--- files/entities/portable_anvil/kick.lua
+++ files/entities/portable_anvil/kick.lua
@@ -1,5 +1,6 @@
 function kick(entity_who_kicked)
   local entity_id = GetUpdatedEntityID()
+  if EntityGetParent(entity_id) > 0 then return end
   local x, y = EntityGetTransform(entity_id)
   EntityLoad("mods/anvil_of_destiny/files/entities/portable_anvil/buildup.xml", x, y - 6)
   GamePlaySound("data/audio/Desktop/projectiles.snd", "player_projectiles/crumbling_earth/create", x, y)
